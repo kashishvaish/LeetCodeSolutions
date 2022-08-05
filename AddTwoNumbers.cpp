cc54c33338792c8747808263eb3ed3d137dbc135
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2022-08-05 12:51:35 +0530

    Update AddTwoNumbers.cpp

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -20,27 +20,17 @@ public:
         ListNode* head = new ListNode(0);
         ListNode* curr = head;
         int carry = 0;
-        while(l1 && l2) {
-            int sum = l1->val + l2->val + carry;
+        while(l1 || l2) {
+            int num1 = l1 ? l1->val : 0;
+            int num2 = l2 ? l2->val : 0;
+            int sum = num1 + num2 + carry;
             curr->next = new ListNode(sum % 10);
             curr = curr->next;
             carry = sum / 10;
-            l1 = l1->next;
-            l2 = l2->next;
-        }
-        while(l1) {
-            int sum = l1->val + carry;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-            carry = sum / 10;
-            l1 = l1->next;
-        }
-        while(l2) {
-            int sum = l2->val + carry;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
-            carry = sum / 10;
-            l2 = l2->next;
+            if(l1)
+                l1 = l1->next;
+            if(l2)
+                l2 = l2->next;
         }
         if(carry) {
             curr->next = new ListNode(carry);
