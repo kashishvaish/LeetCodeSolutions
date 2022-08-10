156034048d0d30fc7c78f44c9f08fcb6ce52ce1d
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2022-08-10 13:13:19 +0530

    Palindrome Number Java Solution

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Time: O(log(n))
+        // Space: O(1)
+        int copy = x;
+        long rev = 0;
+        while(copy > 0) {
+            rev = rev*10 + copy%10;
+            copy /= 10;
+        }
+        return x == rev;
+    }
+}
+
+// Approach:
+
+// Reverse the number --> compare
+// Time: O(log(n))
+// Space: O(1)
