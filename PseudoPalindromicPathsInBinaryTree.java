9eef964c3ccf91878488728ce4750758da440e5f
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2024-01-24 20:15:56 +0530

    Pseudo-Palindromic Paths in a Binary Tree Java Solution

---
+++ PseudoPalindromicPathsInBinaryTree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pseudoPalindromicPaths (TreeNode root) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(n)
+        int result = 0;
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair(root, 0));
+        while(!stack.isEmpty()) {
+            Pair<TreeNode, Integer> curr = stack.pop();
+            TreeNode node = curr.getKey();
+            int freq = curr.getValue();
+            freq ^= 1 << node.val;
+            if(node.left == null && node.right == null) {
+                if((freq & (freq-1)) == 0) result++;
+            }
+            if(node.left != null) stack.push(new Pair(node.left, freq));
+            if(node.right != null) stack.push(new Pair(node.right, freq));
+        }
+        return result;
+    }
+}
+
+// DFS
+// Track frequency of elements in all paths from root node to leaf nodes.
+// If atmost 1 value comes odd number of times in a path, the path can be considered as pseudo-palindromic.
+// Return the total count of pseudo-palindromic paths found.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
+// Bit Manipulation
+// Saving frequencies in a list is space consuming for large trees. To save space, we can compute parity using bitwise operators.
+// We can keep the frequency of digit 1 in the first bit, 2 in the second bit, etc.
+// Hence, the number of elements with odd frequencies can be tracked using, freq ^= (1 << node.val). Here, Left shift operator is used to define the bit, and XOR operator is used to compute the digit frequency. In a path only those bits are visible that appear an odd number of times.
+// Now, to ensure that at most one digit has an odd frequency, check that path is a power of two, i.e., at most one bit is set to one. That could be done by turning off (= setting to 0) the rightmost 1-bit: freq & (freq - 1) == 0. To subtract 1 means to change the rightmost 1-bit to 0 and to set all the lower bits to 1.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
