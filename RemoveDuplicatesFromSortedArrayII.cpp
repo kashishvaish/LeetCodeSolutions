81a421dcfce52d2b320f3ed283edc1f824eb77b5
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2024-01-25 23:44:27 +0530

    Remove Duplicates from Sorted Array II Cpp Solution

---
+++ RemoveDuplicatesFromSortedArrayII.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        int n = nums.size();
+        if(n <= 2) return n;
+        int k = 2;
+        int secondlast = nums[0];
+        int last = nums[1];
+        for(int i = 2; i < n; i++) {
+            if(nums[i] != secondlast) {
+                nums[k] = nums[i];
+                k++;
+            }
+            secondlast = last;
+            last = nums[i];
+        }
+        return k;
+    }
+};
+
+// Approach:
+// The idea is to iterate through the array and maintain a pointer k that keeps track of the position where the next valid element should be placed.
+// Two variables secondlast and last are used to keep track of the last two elements encountered, ensuring that the current element is not a duplicate of the previous two.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
