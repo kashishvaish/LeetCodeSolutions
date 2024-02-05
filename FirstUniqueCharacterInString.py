8e891a230e05e90fe6faa70cd02a53332f8db685
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2024-02-05 20:02:32 +0530

    First Unique Character in a String Python Solution

---
+++ FirstUniqueCharacterInString.py
@@ -0,0 +1,19 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        freq = [0]*26
+        for ch in s: freq[ord(ch)-97] += 1
+        for i in range(len(s)):
+            if freq[ord(s[i])-97] == 1: return i
+        return -1
+
+# Approach:
+# Since string s only contains lowercase english alphabets,
+# We can use an array of size 26 to store the frequency of each alphabet.
+# Traverse over the characters of the string once to track the frequencies.
+# Again, traverse the characters, if frequency of a character is 1, return its index.
+# If no such element is found, return -1. 
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
