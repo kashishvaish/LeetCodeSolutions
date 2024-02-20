f5e9b845480b59fe20237df47981142efd564429
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2024-02-20 18:35:20 +0530

    Missing Number Cpp Solution

---
+++ MissingNumber.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // Time Complexity: O(n)
+        // Space Complexity: O(1)
+        int n = nums.size();
+        int sum = 0;
+        for(int num: nums) sum += num;
+        return (n*(n+1)/2) - sum;
+    }
+};
+
+// Bitwise XOR
+// XOR operation on a number with itself results in 0. 
+// XOR all the numbers from 0 to n and all the numbers in the array, the result will be the missing number.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+
+// Mathematical Formula
+// Use the formula (n*(n+1)/2) to calculate the sum of the first n numbers.
+// Subtract the sum of the array from it.
+// The result is the missing number.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
