e1d7f91a478e17bb8d21bfb2647fe4551a6f3218
Kashish Vaish <70279356+kashishvaish@users.noreply.github.com>
2024-02-22 18:40:00 +0530

    Find the Town Judge Cpp Solution

---
+++ FindTheTownJudge.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        // Time Complexity: O(n + m)
+        // Space Complexity: O(n)
+        vector<int> in(n+1, 0);
+        vector<int> out(n+1, 0);
+        for(auto t: trust) {
+            out[t[0]]++;
+            in[t[1]]++;
+        }
+        for(int i = 1; i <= n; i++) {
+            if(in[i] == n-1 && out[i] == 0) return i;
+        }
+        return -1;
+    }
+};
+
+// Approach:
+// Create two arrays in and out, each of size n+1.
+// The in array tracks the number of people who trust each person, while the out array tracks the number of people trusted by each person.
+// Iterate through each trust relationship [u, v] in the trust array.
+// For each trust relationship, increment the out count for the trusting person u and the in count for the trusted person v.
+// Iterate through each person in the town from 1 to n.
+// Check if the person is trusted by n-1 people (in[i] == n-1) and if the person trusts nobody (out[i] == 0).
+// If both conditions are met for a person, return that person's label as the town judge.
+// If no such person is found after iterating through all people, return -1 to indicate that there is no town judge.
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(n)
